e22d25fdfe30b885485594afb6abdb41c1fa429b
Sametkil <samett.kilicarslan@gmail.com>
2023-05-08 01:12:09 +0300

    kdvHesapla

--- src/b/KdvHesaplama.java
+++ src/b/KdvHesaplama.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class KdvHesaplama {
     public static void main(String[] args) {
 
-        double para,kdvOran=0.18,kdv,kdvsiz;
+        double para,kdvOran=0.18 ,kdv,kdvsiz;
 
         Scanner input=new Scanner(System.in);
 
         System.out.println("Miktar girin : ");
         para=input.nextDouble();
 
-        kdv=para*kdvOran;
-        kdvsiz=(para-kdv);
+        kdv= para * kdvOran ;
+        kdvsiz= ( para-kdv );
 
         System.out.println("KDV : " + kdv);
-        System.out.println("KDV'siz fiyat : "+ kdvsiz);
+        System.out.println("KDV'siz fiyat : " + kdvsiz);
         System.out.println("KDV'li fiyat : " + para);
     }
 }
