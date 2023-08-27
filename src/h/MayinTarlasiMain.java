35aaaee965823eff02686d67b27c815f17465d02
Sametkil <samett.kilicarslan@gmail.com>
2023-08-27 19:21:18 +0300

    mayin Tarlasi

---
+++ src/h/MayinTarlasiMain.java
@@ -0,0 +1,8 @@
+package h;
+
+public class MayinTarlasiMain {
+    public static void main(String[] args) {
+        MayinTarlasi mayinTarlasi=new MayinTarlasi(5,5);
+        mayinTarlasi.run();
+    }
+}
